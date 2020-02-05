7bce377d1308b217bc0381b6cb85fbbb46de10fa
Arman980 <59052832+Arman980@users.noreply.github.com>
2020-02-05 16:58:28 +0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 //! Requiring modules  --  START
-var grass = require("./xot.js");
-var mard = require("./mard.js");
-var xotaker = require("./xotaker.js");
-var car = require("./car.js");
-var Gishatich = require("./gishatich.js");
+var grass = require("./grass.js");
+var ban = require("./ban.js");
+var grassEater = require("./grassEater.js");
+var Water = require("./water.js");
+var predator = require("./predator.js");
 //! Requiring modules  --  END
 
 //! Setting global arrays  --  START
 grassArr = [];
-mardArr = [];
-xotakerArr = [];
-carArr = [];
-GishatichArr = [];
+waterArr = [];
+grassEaterArr = [];
+banArr = [];
+predatorArr = [];
 grassHashiv = 0;
-mardHashiv = 0;
-xotakerHashiv = 0;
-carHashiv = 0;
-GishatichHashiv = 0;
+banHashiv = 0;
+grassHashiv = 0;
+waterHashiv = 0;
+predatorHashiv = 0;
 //! Setting global arrays  -- END
-function random(fr,ft){
-    Math.floor(Math.random(fr,ft))
+function random(fr, ft) {
+    Math.floor(Math.random(fr, ft))
 }
 //! Creating MATRIX -- START
 
@@ -61,62 +61,66 @@ app.get('/', function (req, res) {
     res.redirect('index.html');
 });
 server.listen(3000, function () {
-    console.log("hey");
+    console.log("I am Here");
 
 });
 //! SERVER STUFF END  --  END
 
-
-grassArr = [];
-waterArr = [];
-banArr = [];
-predatorArr = [];
-grassEaterArr = [];
-
-var Grass = require("./Grass.js")
-var grassEater = require("./grassEater.js")
-var water = require("./water.js")
-var ban = require("./ban.js")
-var predator = require("./predator.js")
-
-
-server.listen(3000);
-
-
 function creatingObjects() {
+    matrix = matrixGenerator(20)
     for (var y = 0; y < matrix.length; y++) {
         for (var x = 0; x < matrix[y].length; x++) {
-            if (matrix[y][x] == 2) {
-                var grassEater = new GrassEater(x, y);
-                grassEaterArr.push(grassEater);
-            } else if (matrix[y][x] == 1) {
-                var grass = new Grass(x, y);
-                grassArr.push(grass);
-                grassHashiv++;
+            if (matrix[y][x] == 1) {
+                var gr = new Grass(x, y, 1);
+                grassArr.push(gr);
+            }
+            else if (matrix[y][x] == 2) {
+                var ge = new GrassEater(x, y, 2);
+                geaterArr.push(ge);
+            }
+            else if (matrix[y][x] == 3) {
+                var pe = new Predator(x, y, 3);
+                predatorArr.push(pe);
+            }
+
+            else if (matrix[y][x] == 4) {
+                var wa = new Water(x, y, 5);
+                waterArr.push(wa);
+            }
+            else if (matrix[y][x] == 5) {
+                var ba = new ban(x, y, 6);
+                banArr.push(ba);
             }
         }
     }
 }
+    creatingObjects();
 
-creatingObjects();
-
-function game() {
-    if (grassArr[0] !== undefined) {
+    function drawserver(data) {
         for (var i in grassArr) {
             grassArr[i].mul();
         }
-    }
-    if (grassEaterArr[0] !== undefined) {
-        for (var i in grassEaterArr) {
-            grassEaterArr[i].eat();
+        for (var i in geaterArr) {
+            geaterArr[i].eat();
         }
-    }//! Object to send
+        for (var i in predatorArr) {
+            predatorArr[i].eat();
+        }
+        for (var i in waterArr) {
+            waterArr[i].mul();
+        }
+        for (var i in banArr) {
+            banArr[i].mul();
+        }
 
-    let sendData = {
-        matrix: matrix,
-        grassCounter: grassHashiv
+
+
+
+        let sendData = {
+            matrix: matrix,
+            grassCounter: grassHashiv
+        }
+        //! Send data over the socket to clients who listens "data"
+        io.sockets.emit("data", sendData);
     }
-
-    //! Send data over the socket to clients who listens "data"
-    io.sockets.emit("data", sendData);
-}
+    setInterval(drawserver, 1000); 
